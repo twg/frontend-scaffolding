7d7ac560651ea05734c3b1e7e1ab03a6a9130aa4
themarkappleby <themarkappleby@gmail.com>
2014-09-24 16:12:15 -0400

    passed utilities classes through the replicator

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('css', function() {
       return "Stylus error: " + error.message
     }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    // .pipe(minify())
+    .pipe(minify())
     .pipe(gulp.dest('./dist/css'))
     .pipe(notify('Compiled CSS'))
 
