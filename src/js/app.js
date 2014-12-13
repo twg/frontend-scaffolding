ecd607f28ec3fd55549b44b1e66a27a2463e8acf
Haris Mahmood <harismahmood89@gmail.com>
2014-12-13 01:39:35 -0500

    haris is an idiot, reverting derpness

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
-  $('.icon-mahmoodmagic').delay(1000).fadeIn(2000);
-  $('#intro-quote').delay(1000).fadeIn(2000).delay(3000).fadeOut(2000);
-  $('.intro-text').delay(8500).fadeIn(2000);
+  console.log('Hello World');
 });
